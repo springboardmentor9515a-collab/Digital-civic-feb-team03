0f4a972688b10a7ba0ebdd1b68864a68386350b2
Santhivarshini <santhivarshinidevan@gmail.com>
2026-03-01 09:06:15 -0800

    Remove .next from repository

--- frontend/.next/dev/server/pages/_document.js
+++
@@ -1,5 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_document.js")
-R.c("server/chunks/ssr/8bb4e_5dafcd66._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
-R.m("[project]/civic-project/frontend/node_modules/next/document.js [ssr] (ecmascript)")
-module.exports=R.m("[project]/civic-project/frontend/node_modules/next/document.js [ssr] (ecmascript)").exports
--- frontend/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"4uMziHJ3+no16F8tANVXZdrxytlRFVFynhioFnGueas=\"\n}"
