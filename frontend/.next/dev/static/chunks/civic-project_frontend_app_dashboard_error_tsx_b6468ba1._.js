2c60d57b432b182ce87a1534f24167f846431fe8
Santhivarshini <santhivarshinidevan@gmail.com>
2026-03-01 08:58:28 -0800

    Final full stack project clean commit

---
+++ frontend/.next/dev/static/chunks/civic-project_frontend_app_dashboard_error_tsx_b6468ba1._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/civic-project_frontend_53c6fde9._.js"
+],
+    source: "dynamic"
+});
---
+++ frontend/.next/dev/static/chunks/civic-project_frontend_app_petitions_[id]_page_tsx_b6468ba1._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/civic-project_frontend_810781f7._.js"
+],
+    source: "dynamic"
+});
